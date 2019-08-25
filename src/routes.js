add6bb274a84153d6f71f68833dfa1a4e1fc0b41
Pedro Nascimento <nennogabriel@gmail.com>
2019-08-25 02:11:58 -0300

    0209 loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/Users';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Rocketseat' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diegofernandes@rocketseat.com.br',
+    password_hash: '123456123456',
+  });
+  return res.json(user);
+});
 
 export default routes;
