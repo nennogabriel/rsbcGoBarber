fa71e5f6a3e079d419a3c19d6a85d77a7cd254f9
Pedro Nascimento <nennogabriel@gmail.com>
2019-08-26 22:37:29 -0300

    03a08 Scheduler list

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -22,5 +23,6 @@ routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
