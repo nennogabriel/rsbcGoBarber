857e973cc2301936637f08349083da74bddac37b
Pedro Nascimento <nennogabriel@gmail.com>
2019-08-27 22:09:47 -0300

    03a14 sending an email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4f4c272c62228b',
+    pass: '74ab8d6a24ad3f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
